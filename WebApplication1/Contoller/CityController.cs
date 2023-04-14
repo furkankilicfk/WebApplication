2023d5b827f37449d104e7aeb2c590501cb1c9b6
Furkan Kılıç <furkankilicfk@outlook.com>
2023-04-15 02:36:07 +0300

    View Configuration and Data Transport Controls to the View(Model,ViewBag,ViewData,TempData)

---
+++ WebApplication1/Contoller/CityController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using WebApplication1.Models;
+
+namespace WebApplication1.Contoller
+{
+    public class CityController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var cities = new List<City>
+            {
+                new City { Id = 1, CityName = "Paris", Population = 100000 },
+                new City { Id = 2, CityName = "Munchen", Population = 60000 },
+                new City { Id = 2, CityName = "Istanbul", Population = 5000000 },
+
+
+            };
+            //Model Bazlı Veri Gönderimi
+            //return View(cities); //göndereceğimiz datayı da bildiriyoruz 
+
+
+            //ViewBag
+            //View'e gönderilecek/taşınacak datayı dynamic şekilde tanımlanan bir değişkenle taşımamızı sağlayan bir veri taşıma kontrolüdür.
+
+            //ViewBag.data = cities;          //Dynamic değişkene elimdeki datayı verdim  ||  view'de erişilebilir hale geldi
+
+            //return View();
+
+            //ViewData 
+            //ViewBag'de old gibi actiondaki datayı view'e taşımamızı sağlayan bir kontroldür. Viewdata ilgili datayı boxing ederek taşır
+
+            ViewData["data"] = cities;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/City.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+
+        public string CityName { get; set; }
+
+        public int Population { get; set; }
+    }
+}
